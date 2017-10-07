34a56dc9bcbe67ff0e3b724dc46ebf83e58ae7b9
Nguyen Dinh Duc <dinhduc0605@gmail.com>
2017-10-07 17:37:27 +0700

    change saved manga location and css path

--- manga.rb
+++ manga.rb
@@ -20,18 +20,18 @@ def show_menu
   choice = gets.chomp
   case choice
     when "1"
-      p "------------------ Crawl manga form SenManga ---------------------"
-      p "Input manga name: "
+      print "------------------ Crawl manga form SenManga ---------------------\n"
+      print "Input manga name: "
       manga_name = gets.chomp
       crawl_manga manga_name
     when "2"
-      p "------------------ Convert from images to HTML -------------------"
-      p "Input manga folder path: "
+      print "------------------ Convert from images to HTML -------------------\n"
+      print "Input manga folder path: "
       manga_folder_path = gets.chomp
       convert_to_html manga_folder_path
     when "3"
-      p "--------------------- Rename image file --------------------------"
-      p "Input manga folder path: "
+      print "--------------------- Rename image file --------------------------\n"
+      print "Input manga folder path: "
       # path phai dung / khong duoc dung \
       manga_folder_path = gets.chomp
       rename_image_file manga_folder_path
@@ -46,7 +46,7 @@ def download_photo(photo_url, filepath)
       File.open(filepath, "w") { |file| file.puts f.read }
     end
   rescue Exception => e
-    p "Error_download_photo: #{e}"
+    print "Error_download_photo: #{e}"
   else
     print "Download successful, saved to #{filepath}\n\n"
   end
@@ -55,7 +55,7 @@ end
 # crawl from sen manga
 def crawl_manga manga_name
   processed_manga_name = manga_name.gsub(" ", "_")
-  p "-------------------- Crawl Manga: #{manga_name}---------------------------"
+  print "-------------------- Crawl Manga: #{manga_name}---------------------------\n"
   # FileUtils.makedirs(root_folder) unless File.exist?(ROOT_FOLDER)
   manga_page = Nokogiri::HTML open("#{ROOT_URL}/#{processed_manga_name}")
   manga_folder_path = "#{ROOT_FOLDER}/#{manga_name}"
@@ -80,16 +80,23 @@ def crawl_manga manga_name
 end
 
 def convert_to_html manga_folder_path
-  index_file_content = '<!DOCTYPE html>
+  current_dir_path = Dir.pwd
+  p current_dir_path
+  manga_dir = Dir.open manga_folder_path
+  manga_name = File.basename manga_dir
+  p manga_name
+  manga_html_dir_path = "#{current_dir_path}/#{manga_name}"
+  FileUtils.makedirs(manga_html_dir_path) unless File.exist?(manga_html_dir_path)
+  index_file_content = "<!DOCTYPE html>
                         <html>
                         <head>
                           <title>Mangas</title>
-                          <link rel="stylesheet" type="text/css" href="custom.css">
+                          <link rel='stylesheet' type='text/css' href='#{current_dir_path}/custom.css'>
 
                         </head>
                         <body>
                           <h1>Chapter List</h1>
-                          <ul>'
+                          <ul>"
   chapter_dirs = Dir.glob("#{manga_folder_path}/*").select {|f| File.directory? f}
 
   chapter_dirs.each do |dir|
@@ -101,7 +108,7 @@ def convert_to_html manga_folder_path
                           <html>
                           <head>
                             <title>#{chapter_name}</title>
-                            <link rel=\"stylesheet\" type=\"text/css\" href=\"custom.css\">
+                            <link rel='stylesheet' type='text/css' href='#{current_dir_path}/custom.css'>
                           </head>
                           <body>
                             <h1>#{chapter_name}</h1>"
@@ -112,26 +119,26 @@ def convert_to_html manga_folder_path
     end
     chapter_file_content += ' </body>
                               </html>'
-    chapter_file = File.open("#{chapter_name}.html", 'w+')
+    chapter_file = File.open("#{manga_html_dir_path}/#{chapter_name}.html", 'w+')
     chapter_file.write chapter_file_content
     chapter_file.close
   end
   index_file_content += '</ul>
                       </body>
                       </html>'
-  index_file = File.open('index.html', 'w+')
+  index_file = File.open("#{manga_html_dir_path}/index.html", 'w+')
   index_file.write index_file_content
   index_file.close
 end
 
 def rename_image_file manga_folder_path
-  p manga_folder_path
+  print manga_folder_path
   chapter_dirs = Dir.glob("#{manga_folder_path}/*").select {|f| File.directory? f}
   chapter_dirs.each do |dir|
     # ten folder ko dc co dau []
     dir_name = File.basename dir
     chapter_path = "#{manga_folder_path}/#{dir_name}"
-    p chapter_path
+    print chapter_path
     image_files = Dir.glob("#{chapter_path}/*")
     image_files.each_with_index do |file, index|
       File.rename(file, chapter_path + "/" + ("%03d" % index) + File.extname(file))
